cee20d8f293e15d14555eb0701725d594a52dfdf
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-05-14 07:49:55 -0700

    design and settings pages structure in place

--- shop_happy/apps/app_settings/urls.py
+++ shop_happy/apps/app_settings/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
+
+from shop_happy.decorators import shop_login_required
 
 from views import SettingsView, DesignView
 
+
 urlpatterns = patterns('',
-    
-    url(r'^settings/$', SettingsView.as_view(), name='settings'),
-    url(r'^design/$', DesignView.as_view(), name='design'),
+    url(r'^settings/$', shop_login_required(SettingsView.as_view()), name='settings'),
+    url(r'^design/$', shop_login_required(DesignView.as_view()), name='design'),
+
 )
--- shop_happy/apps/app_settings/views.py
+++ shop_happy/apps/app_settings/views.py
@@ -5,13 +5,12 @@ from django.shortcuts import render_to_response, redirect
 from django.core.urlresolvers import reverse
 from django.views.generic.base import TemplateView, RedirectView
 
-
 import shopify
-
 
 
 class SettingsView(TemplateView):
     template_name = 'app_settings/settings.html'
+
 
 class DesignView(TemplateView):
     template_name = 'app_settings/design.html'
