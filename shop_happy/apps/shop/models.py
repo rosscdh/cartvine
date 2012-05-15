ddbb0478834b2f7065fa5b8d70bfafec9a332663
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-05-15 00:53:48 -0700

    Added index

--- shop_happy/apps/shop/models.py
+++ shop_happy/apps/shop/models.py
@@ -4,8 +4,8 @@ from shop_happy.fields import JSONField
 
 class Shop(models.Model):
     name = models.CharField(max_length=255)
-    shopify_id = models.IntegerField()
-    slug = models.SlugField()
+    shopify_id = models.IntegerField(db_index=True)
+    slug = models.SlugField(db_index=True)
     url = models.URLField()
     data = JSONField(null=True)
 
