f6d14560265dd3f88fed7d685f090997ed15dbbb
Ross Crawford <sendrossemail@gmail.com>
2012-05-15 12:57:14 -0700

    live static change

--- conf/live.local_settings.py
+++ conf/live.local_settings.py
@@ -4,11 +4,11 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__file__+ '/../'))
 
 STATIC_URL = '/static/shopify/'
 
-STATIC_ROOT = '/home/stard0g101/webapps/static/shopify'
+STATIC_ROOT = '/home/stard0g101/webapps/static/shop_happy/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    '/home/stard0g101/webapps/static/shopify/base/',
+    '/home/stard0g101/webapps/static/shop_happy/base/',
 )
 
 # Shopify Key
