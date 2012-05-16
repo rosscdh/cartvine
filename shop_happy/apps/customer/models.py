d3a216d0919496ea5bf6c751224dcdb473d77113
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-05-15 22:14:07 -0700

    Customer and Mail Models

--- shop_happy/apps/customer/models.py
+++ shop_happy/apps/customer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from shop_happy.apps.shop.models import Shop
 
-# Create your models here.
+
+class Customer(models.Model):
+    shops = models.ManyToManyField(Shop, related_name='shops')
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField()
--- shop_happy/apps/mail/models.py
+++ shop_happy/apps/mail/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from shop_happy.apps.shop.models import Shop
+from shop_happy.apps.customer.models import Customer
 
-# Create your models here.
+
+class Email(models.Model):
+    shop = models.ForeignKey(Shop)
+    customer = models.ForeignKey(Customer, related_name='target_customer')
+    email_to = models.EmailField()
+    body = models.TextField()
