06da31502ba41838c757b873a0c38e2003eb7296
Ross Crawford <sendrossemail@gmail.com>
2012-05-22 11:57:26 -0700

    working template tag needs tests

--- shop_happy/apps/product/templatetags/product_tags.py
+++ shop_happy/apps/product/templatetags/product_tags.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
+from urlparse import urlparse
 
 register = template.Library()
 
@@ -11,7 +12,9 @@ def image_resize(src, size='large'):
     """
     Reformat the image name to have <image_name>_SIZE.jpg in it
     """
-    image = src.split('.')
-    src = '%s_%s.%s' % (image[0],size,image[1],)
+    image = urlparse(src)
+    path = image.path.split('.')
+    path = '%s_%s.%s' % (path[0],size,path[1],)
+    src = '%s://%s%s?%s' %(image.scheme, image.netloc, path, image.params)
     return src
 image_resize.is_safe = True
