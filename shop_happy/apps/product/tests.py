e2f3998d66ceebecb0c1cb79f5802bc772eae9b4
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-05-21 22:27:56 -0700

    working on product view test.. failing atm

--- shop_happy/apps/product/tests.py
+++ shop_happy/apps/product/tests.py
@@ -4,7 +4,7 @@ Test the default app views
 from django.contrib.auth.models import User
 from django.test.client import Client
 from django.test import TestCase
-
+from django.db.models.query import QuerySet
 from django.core.urlresolvers import reverse
 
 from shop_happy.apps.default.forms import ShopifyInstallForm
@@ -20,6 +20,22 @@ login_required_urls = [
 class ProductTest(TestCase):
     def setUp(self):
         self.client = Client()
+        # Setup fixtures
+        # create user
+        User.objects.create_user('usera', 'usera@test.com', 'test')
+        user_a = User.objects.get(username='usera')
+        shop_a = Shop.objects.create(name='shop a',shopify_id=1234567, shopify_access_token=1234567,slug='shop-a',url='shop-a.myshopify.com')
+        shop_a.users.add(user_a)
+        # create user products and associate with user1
+        self.create_products_for_shop(shop_a)
+
+        # create second user
+        User.objects.create_user('userb', 'userb@test.com', 'test')
+        user_b = User.objects.get(username='usera')
+        shop_b = Shop.objects.create(name='shop b',shopify_id=2345678, shopify_access_token=2345678,slug='shop-b',url='shop-b.myshopify.com')
+        shop_b.users.add(user_b)
+        # create second user products and assocaited with user2
+        self.create_products_for_shop(shop_b)
 
     def test_anonymous_cannot_access_logged_in_urls(self):
         """ test primary logged in urls cannot be acessed unless logged in """
@@ -31,28 +47,20 @@ class ProductTest(TestCase):
 
     def test_product_list_view(self):
         """ test that when logged in the user can see their list of products (only their products) """
-        # create user
-        shop_a = Shop.objects.create(name='shop a',shopify_id=1234567, shopify_access_token=1234567,slug='shop-a',url='shop-a.myshopify.com')
-        user_a = User.objects.create(username='usera', email='usera@test.com')
-        shop_a.users.add(user_a)
-        # create user products and associate with user1
-        self.create_products_for_user(shop_a)
-        # create second user
-        shop_b = Shop.objects.create(name='shop b',shopify_id=2345678, shopify_access_token=2345678,slug='shop-b',url='shop-b.myshopify.com')
-        user_b = User.objects.create(username='userb', email='userb@test.com')
-        shop_b.users.add(user_b)
-        # create second user products and assocaited with user2
-        self.create_products_for_user(shop_b)
         # log user 1 in
+        loggedin = self.client.login(username='usera', password='test')
+
         # test user1 can see products
         # test user1 can see only their products
         response = self.client.get(reverse('product:index'))
-        print response.status_code
 
-    def create_products_for_user(self, shop, num_products=5):
+        self.assertTrue(isinstance(response.context['object_list'], QuerySet))
+        self.assertEqual(response.context['object_list'].count(), 5)
+
+    def create_products_for_shop(self, shop, num_products=5):
         """ Create X number of products for the provided shop object """
         product_list = []
-        for i in range(1,num_products):
+        for i in range(1,num_products+1):
             shopify_id = (i*1000)
             name = 'Product %d for Shop %s' %(i,shop.name,)
             data = {
