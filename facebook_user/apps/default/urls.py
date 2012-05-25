c3dff5772d277053dee458c130afba8dddb832d3
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-05-24 22:18:36 -0700

    removed references to old direct_to_template view with TemaplteView

--- facebook_user/apps/default/urls.py
+++ facebook_user/apps/default/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 from shop_happy.decorators import shop_login_required
 
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', login_required(LogoutView.as_view()), name='logout'),
     # Facebook Specific Urls
-    url(r'^facebook_debug/', direct_to_template, {'template':'facebook_debug.html'}, name='fb-debug'),
-    url('^channel\.html', direct_to_template, {'template':'channel.html'}, name='facebook_channel_receiver'),
+    url(r'^facebook_debug/', TemplateView.as_view(template='facebook_debug.html'), name='fb-debug'),
+    url('^channel\.html', TemplateView.as_view(template='channel.html'), name='facebook_channel_receiver'),
     url(r'^$', DefaultView.as_view(), name='index'),
 )
