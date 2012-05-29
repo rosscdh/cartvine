5f20a80133c517b50a28abb2e74394dc92e9dc73
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-05-29 16:45:59 +0200

    Added middleware to handle xss for js files and added the widgets functions

--- facebook_user/apps/default/static/widgets/cartvine-loader.js
+++ facebook_user/apps/default/static/widgets/cartvine-loader.js
@@ -8,16 +8,12 @@ url_scheme = '//';
 widget_list_object = url_scheme + 'localhost:8000/widgets/'+ shop_slug +'/'
 
 $.getJSON(widget_list_object, function(data) {
-    var items = []
-  $.each(data, function(key, val) {
-    console.log(key)
-    items.push('<li id="' + key + '">' + val + '</li>');
-  });
-
-  $('<ul/>', {
-    'class': 'my-new-list',
-    html: items.join('')
-  }).appendTo('body');
+    $.each(data.widgets, function(index) {
+        $('<script/>', {
+            src: data.widgets[index]
+        }).appendTo('head');
+        console.log(data.widgets[index])
+    });
 });
 
 var oHead = document.getElementsByTagName('BODY').item(0);
@@ -32,21 +28,21 @@ var oScript = '';
 // oScript = '';
 
 
-scripts = {
-    'widgets': ['http://localhost:8001/static/widgets/widget-auth-facebook.js']
-};
-// <!-- setup local js object to load the provided remote scripts --> 
+// scripts = {
+//     'widgets': ['http://localhost:8001/static/widgets/widget-auth-facebook.js']
+// };
+// // <!-- setup local js object to load the provided remote scripts --> 
 
 
-// loop over provided script urls and install them locally
-if (scripts['widgets'].length > 0) {
-    for (index in scripts['widgets']) {
-        remote_script = scripts['widgets'][index];
+// // loop over provided script urls and install them locally
+// if (scripts['widgets'].length > 0) {
+//     for (index in scripts['widgets']) {
+//         remote_script = scripts['widgets'][index];
 
-        oScript = document.createElement("script");
-        oScript.type = "text/javascript";
-        oScript.src = remote_script;
+//         oScript = document.createElement("script");
+//         oScript.type = "text/javascript";
+//         oScript.src = remote_script;
 
-        oHead.appendChild(oScript);
-    };
-}
+//         oHead.appendChild(oScript);
+//     };
+// }
