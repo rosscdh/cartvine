4ca68ccd386afd29b790dea7f0c2bcf903fe4fa1
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-05-30 09:31:41 +0200

    experimenting with .ajax caching

--- cartvine/apps/widget/templates/widget/cartvine-loader.js
+++ cartvine/apps/widget/templates/widget/cartvine-loader.js
@@ -6,26 +6,28 @@ var shop_slug = window.Shopify.shop.replace('.myshopify.com','');
 var url_scheme = window.parent.document.location.protocol + '//';
 
 // <!-- load required js files from shopify.app.cartvine.com/widgets/shop-url-extracted-from-shopify-store-page-object/: will return a json object of js files to load -- >
-widget_list_object = url_scheme + '{{ request.get_host }}/widget/'+ shop_slug +'/'
+widget_list_object = url_scheme + '{{ request.get_host }}/widget/'+ shop_slug +'/';
+//widget_list_object = url_scheme + 'shopify.app.cartvine.com/widget/'+ shop_slug +'/';
 
 $.getJSON(widget_list_object, function(data) {
     // Install Templates
     $.each(data.templates, function(index) {
         $(data.templates[index]).appendTo('head');
     });
+
     // Install Scripts
+    $.ajaxSetup({ cache: true });
+
     $.each(data.widgets, function(index) {
         $.ajax({
             url: data.widgets[index],
             dataType: 'script',
+            cache: true,
             complete: function(xhr) { 
                 if (xhr.status == 304) return;
-                // or better: if (xhr.status != 200) return;
-                // your code goes here
             },
             error: function () {
-            // cartvine_is_ready = false;
-            // // something happened and we cannto go on like this
+                cartvine_is_ready = false;
             }
         });
         // $.getScript(data.widgets[index])
