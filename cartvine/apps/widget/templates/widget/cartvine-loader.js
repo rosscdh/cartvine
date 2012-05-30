429809420ba70308fe75511b7a7d3e3e18ac09aa
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-05-30 09:33:42 +0200

    experimenting with .ajax caching

--- cartvine/apps/widget/templates/widget/cartvine-loader.js
+++ cartvine/apps/widget/templates/widget/cartvine-loader.js
@@ -5,9 +5,7 @@ var cartvine_is_ready = true;
 var shop_slug = window.Shopify.shop.replace('.myshopify.com','');
 var url_scheme = window.parent.document.location.protocol + '//';
 
-// <!-- load required js files from shopify.app.cartvine.com/widgets/shop-url-extracted-from-shopify-store-page-object/: will return a json object of js files to load -- >
 widget_list_object = url_scheme + '{{ request.get_host }}/widget/'+ shop_slug +'/';
-//widget_list_object = url_scheme + 'shopify.app.cartvine.com/widget/'+ shop_slug +'/';
 
 $.getJSON(widget_list_object, function(data) {
     // Install Templates
@@ -30,11 +28,5 @@ $.getJSON(widget_list_object, function(data) {
                 cartvine_is_ready = false;
             }
         });
-        // $.getScript(data.widgets[index])
-        // .done(function(script, textStatus) {})
-        // .fail(function(jqxhr, settings, exception) {
-        //     cartvine_is_ready = false;
-        //     // something happened and we cannto go on like this
-        // });
     });
 });
