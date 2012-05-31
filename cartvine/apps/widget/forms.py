8b89f9bb12f973f40581ec6a6f6597fdc04b3ca0
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-05-31 09:28:09 -0700

    Added basic widget purchase process

--- cartvine/apps/widget/forms.py
+++ cartvine/apps/widget/forms.py
@@ -7,7 +7,7 @@ from models import Widget
 
 
 class BaseWidgetEditForm(bootstrap.BootstrapForm):
-    target_id = forms.CharField(_('Target ID'), help_text=_('The div#id_name or the html element name that you want to inject your widget into'), required=True, initial='body', widget=bootstrap_widgets.PrependedText)
+    target_id = forms.CharField(_('Target ID'), help_text=_('The div#id_name or the html element name that you want to inject your widget into'), required=True, initial='body', widget=bootstrap_widgets.PrependedText(attrs={'prepend_text': '#'}))
 
     class Meta:
         layout = (bootstrap.Fieldset("Configure your widget", "target_id"),)
