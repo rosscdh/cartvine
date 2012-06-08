cdf737ca13876f07bb3f2afa332753cc9f02406b
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-06-08 11:04:13 +0200

    test console logs

--- cartvine/apps/widget/templates/widget/widget-prop-plus.js
+++ cartvine/apps/widget/templates/widget/widget-prop-plus.js
@@ -6,13 +6,22 @@ this.{{ widget.widget_js_name }} = function() {
     this.App.productsView = Em.View.create({
       template: Em.Handlebars.compile("<h1>PROJECTS</h1><p>State: {{this.App.routeManager.currentState.path}}</p>")
     });
+    this.App.cartView = Em.View.create({
+      template: Em.Handlebars.compile("<h1>CART VIEW</h1><p>State: {{this.App.routeManager.currentState.path}}</p>")
+    });
 
     this.App.routeManager = Em.RouteManager.create({
-        products: Em.ViewState.create({}),
-        projects: Em.ViewState.create({
-        route: 'products',
-        view: this.App.productsView
+        products: Em.ViewState.create({
+            route: 'products',
+            enter: function(stateManager, transition) {
+                console.log('Entered Ember products view')
+            }
         }),
-        cart: Em.ViewState.create({}),
+        cart: Em.ViewState.create({
+            route: 'cart',
+            enter: function(stateManager, transition) {
+                console.log('Entered Ember cart view')
+            }
+        }),
     });
 },
