87d24a8e8913652972f899272f617b301d1b8160
Ross Crawford <sendrossemail@gmail.com>
2012-06-24 17:53:13 +0200

    davis workingbut not in ff?

--- cartvine/apps/widget/templates/widget/for_shop.js
+++ cartvine/apps/widget/templates/widget/for_shop.js
@@ -1,84 +1,95 @@
-
-// CV object
-var CartVine = function() {
-    var self = this;
-
-    this.shop = "{{ object.name }}",
-    this.shop_url = "{{ object.url }}",
-    this.cartvine_shop_id = "{{ object.pk }}",
-    this.slug = "{{ object.slug }}",
-    this.widgets = [{% for s in scripts %}"{{ s }}"{% if not forloop.last %},{% endif %}{% endfor %}],
-    this.templates = [{% for t in templates %}"{{ t|escapejs }}"{% if not forloop.last %},{% endif %}{% endfor %}]
-    this.App = void 0,
-    this.DS = void 0,
-
-    this.injectView = function(view, widget_name, target) {
-    	target_ob = $(target);
-	    if (target_ob.length <= 0 || target == 'body') {
-	        // view insert target not found
-			$('body').append(view);
-	    }else{
-            target_ob.append(view);
-	    }
-    }
-
-    this.init = function () {
-    	var self = this;
-
-	    // Install Templates
-	    $.each(this.templates, function(index) {
-	        $('head').append($(self.templates[index]));
-	    });
-
-    	// Install Scripts
-    	// @TODO combine this
-    	var num_widgets = this.widgets.length;
-    	var loaded_widgets = 0;
-    	$.each(this.widgets, function(index) {
-    		var url = self.widgets[index];
-
-    		$.ajaxSetup({ cache: true });
-
-	        $.ajax({
-	            url: url,
-	            dataType: 'script',
-	            cache: true,
-	            complete: function(xhr) { 
-	                loaded_widgets++;
-
-                    if (Davis != 'undefined') {
-                        self.App = Davis(function () {});
-                    }
-
-                    if (loaded_widgets == num_widgets) {
-                        self.loadWidgets();
-                    }
-
-	                if (xhr.status == 304) return;
-	            },
-	            error: function () {}
-	        });
-
-    	});
-    }
-
-    this.loadWidgets = function() {
+//$(document).ready(function () {
+    // CV object
+    var CartVine = function() {
         var self = this;
-		// complete init
-		{% spaceless %}{% for widget in widget_list_init_names %}
-		//self.{{ widget }}();
-		{% endfor %}{% endspaceless %}
 
-        // Output of combined widgets
-        {{ combined_widgets|safe }}
-        // /End Output of combined widgets
+        this.App = void 0,
+        this.url_scheme = window.parent.document.location.protocol + '//',
+        this.shop = "{{ object.name }}",
+        this.shop_url = "{{ object.url }}",
+        this.cartvine_shop_id = "{{ object.pk }}",
+        this.slug = "{{ object.slug }}",
+        this.widgets = [{% for s in scripts %}"{{ s }}"{% if not forloop.last %},{% endif %}{% endfor %}],
+        this.templates = [{% for t in templates %}"{{ t|escapejs }}"{% if not forloop.last %},{% endif %}{% endfor %}]
 
-        self.App.start();
-    }
-};
+        this.buildUrl = function (path) {
+            if (path.substr(0,1) == '/') {
+                path = path.substr(1);
+            }
+            url = self.url_scheme + '{{ cartvine_sites.shopify_app_domain }}/';
+            request_url = url + path;
+            return request_url;
+        }
 
-// cv instance
-var cartvine = new CartVine();
-cartvine.init();
+        this.injectView = function(view, widget_name, target) {
+            target_ob = $(target);
+            if (target_ob.length <= 0 || target == 'body') {
+                // view insert target not found
+                $('body').append(view);
+            }else{
+                target_ob.append(view);
+            }
+        }
+
+        this.init = function () {
+            var self = this;
+
+            // Install Templates
+            $.each(this.templates, function(index) {
+                var template = $.trim(self.templates[index]);
+                if (template.length > 0) {
+                   $('head').append(template);
+                }
+            });
+
+            // Install Scripts
+            // @TODO combine this
+            var num_widgets = this.widgets.length;
+            var loaded_widgets = 0;
+            $.each(this.widgets, function(index) {
+                var url = self.widgets[index];
+
+                $.ajaxSetup({ cache: true });
+
+                $.ajax({
+                    url: url,
+                    dataType: 'script',
+                    cache: true,
+                    complete: function(xhr) { 
+                        loaded_widgets++;
+
+                        if (loaded_widgets == num_widgets) {
+                            self.loadWidgets();
+                        }
+
+                        if (xhr.status == 304) return;
+                    },
+                    error: function () {}
+                });
+
+            });
+        }
+
+        this.loadWidgets = function() {
+            var self = this;
+            if (Davis != 'undefined') {
+
+                self.App = Davis(function () {
+                    var d = this;
+
+                    // Start app Url
+                    {{ combined_widgets|safe }}
+
+                });
+
+                self.App.start();
+            };
+        }
+    };
+
+    // cv instance
+    var cartvine = new CartVine();
+    cartvine.init();
 
 
+//});
--- cartvine/apps/widget/templates/widget/widget-prop-plus.js
+++ cartvine/apps/widget/templates/widget/widget-prop-plus.js
@@ -1,11 +1,37 @@
 {% load url from future %}
-//this.{{ widget.widget_js_name }} = function() {
+this.get('/products/:slug', function (req) {
 
-Davis.greeter = function () {
-    this.get('/greet/:name', function (req) {
-    alert("Hello " + req.params['name'])
+    var slug = req.params['slug'].replace('.html', '');
+    var url = self.buildUrl('/api/v1/product/?slug=' + slug);
+
+    $.getJSON(url, function(data) {
+        product = data.objects[0];
+
+        var variationControlsView = function() {
+            var source   = $("script#variation-controls").html();
+            var context = {
+                'product': product,
+            };
+            var template = Handlebars.compile(source);
+            return template(context);
+        };
+
+        self.injectView(variationControlsView(), '{{ widget.widget_js_name }}', '{{ config.variations.target_id }}');
+
+        // list of properties
+        var url = self.buildUrl('/api/v1/variant/?product=' + product.id);
+        $.getJSON(url, function(data) {
+            variant_list = data.objects;
+            var productPropertiesView = function(sam) {
+                var source   = $("script#product-properties").html();
+                var context = {
+                    'product': product,
+                    'variant_list': variant_list,
+                };
+                var template = Handlebars.compile(source);
+                return template(context);
+            };
+            self.injectView(productPropertiesView(), '{{ widget.widget_js_name }}', '{{ config.products.target_id }}');
+        })
     })
-}
-
-
-//}
+})
